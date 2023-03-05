d2f2d7d60b0fb4ad0101bcfe6ee83cf5299c36f8
Denis Moiseev <isdenmois@gmail.com>
2023-03-05 11:00:26 +0300

    fix: slash in the file name

--- src/renderer/features/rename/model/rename-files.ts
+++ src/renderer/features/rename/model/rename-files.ts
@@ -3,24 +3,32 @@ import { atom } from 'nanostores'
 import { folderAPI, type QFile } from 'shared/api'
 import { selectedFolder$ } from 'entities/folder'
 import { selectedSeries$ } from 'entities/series'
-import { season$ } from './files'
 
 export const isRenaming$ = atom(false)
 const IS_MOVIE = /\.(mkv|mp4|avi|webm|flv|mov|m4v|mpg|mpeg|mov)$/i
 
+const getFileDestination = (file: QFile, seriesName: string) => {
+  const title = file.title.replaceAll('/', '-')
+
+  if (file.filename.match(IS_MOVIE)) {
+    return `${seriesName}/Season ${file.season}/${title}`
+  }
+
+  return `${file.directory}/${title}`
+}
+
 export const renameFiles = async (files: QFile[]) => {
   const { seriesName } = selectedSeries$.get()
   const hash = selectedFolder$.get().hash
-  const season = season$.get()
-  const kodiFolder = `${seriesName}/Season ${season}`
 
   isRenaming$.set(true)
 
   for (const file of files) {
     if (file.title && file.title !== file.filename) {
-      const prefix = file.filename.match(IS_MOVIE) ? kodiFolder : file.directory
+      const from = `${file.directory}/${file.filename}`
+      const to = getFileDestination(file, seriesName)
 
-      await folderAPI.rename(hash, `${file.directory}/${file.filename}`, `${prefix}/${file.title}`)
+      await folderAPI.rename(hash, from, to)
     }
   }
 
