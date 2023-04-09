c40c026596bfb28f660011a3c914b734d9898c21
Denis Moiseev <isdenmois@gmail.com>
2023-04-09 14:34:14 +0300

    fix: another attempt to fix PACKAGE_VERSION env variable

--- src/renderer/shared/lib/env/index.ts
+++ src/renderer/shared/lib/env/index.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const schema = z.object({
   PROD: z.coerce.boolean(),
-  PACKAGE_VERSION: z.string(),
+  PACKAGE_VERSION: z.string().transform(version => version.replaceAll('"', '')),
   VITE_TVDB_TOKEN: z.string().min(1),
   VITE_DEFAULT_PORT: z.coerce.number().min(1),
   VITE_GITHUB_REPO: z.string(),
